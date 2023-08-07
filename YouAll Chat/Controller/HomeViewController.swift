3501592c2571127822b4d1ade35e02d4dcc016ff
umer-se <140389967+umer-se@users.noreply.github.com>
2023-08-07 16:23:01 +0500

    not working with mulitipleImageviews swift package so deleting it

--- YouAll Chat/Controller/HomeViewController.swift
+++ YouAll Chat/Controller/HomeViewController.swift
@@ -6,23 +6,48 @@
 //
 
 import UIKit
-import Firebase
+import MultipleImageView
 
-class HomeViewController : UIViewController {
+class HomeViewController : UIViewController{
+
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     //MARK: - Variables
     
     let userAutentication = UserAuthentication()
+    let postSource = Post()
     
-
+     var sources: [MultipleImageView.Source] = [
+        .uiimage(UIImage(systemName: "a.book.closed")!),
+        .uiimage(UIImage(systemName: "a.book.closed")!),
+        .uiimage(UIImage(systemName: "a.book.closed")!),
+        .uiimage(UIImage(systemName: "a.book.closed")!),
+        .uiimage(UIImage(systemName: "a.book.closed")!),
+        .uiimage(UIImage(systemName: "a.book.closed")!)
+    ]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
-
+        tableView.register(UINib(nibName: "Post" , bundle: nil), forCellReuseIdentifier:"postIdentifier")
+        
         
     }
     
+    @IBAction func refreshPressed(_ sender: UIButton) {
+        tableView.reloadData()
+        
+        
+    }
     @IBAction func logOutPressed(_ sender: Any) {
         
         userAutentication.logOut()
@@ -30,3 +55,32 @@ class HomeViewController : UIViewController {
     }
     
 }
+//MARK: - tableView datasource
+
+extension HomeViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postIdentifier" , for: indexPath) as! Post
+    
+        cell.userImage.image = UIImage(systemName: "a.book.closed")
+        
+       // cell.multipleImageView.sources = sources
+        
+        return cell
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
--- YouAll Chat/Views/Post.swift
+++ YouAll Chat/Views/Post.swift
@@ -5,4 +5,79 @@
 //  Created by Umer on 07/08/2023.
 //
 
-import Foundation
+import UIKit
+import MultipleImageView
+
+
+
+class Post : UITableViewCell{
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var postDescription: UILabel!
+    
+    @IBOutlet weak var multipleImageView: UIView!{
+        didSet {
+            multipleImageView.layer.cornerRadius = 20
+            multipleImageView.layer.masksToBounds = true
+           // multipleImageView.delegate = self
+        }
+    }
+    
+    
+    
+    
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //
+//        multipleImageView.sources = sources
+//        multipleImageView.reloadData()
+        //
+        
+    }
+    
+    
+    //MARK: - IBActions
+    
+    @IBAction func likeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func commentButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
+extension Post : MultipleImageViewDelegate{
+    
+    func multipleImageViewShouldGetImage(_ imageView: UIImageView, sourceForURL url: URL, index: Int) {
+        
+        //multipleImageView.delegate = self
+       
+        DispatchQueue.global().async {
+            let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+                guard let data = data, let image = UIImage(data: data) else { return }
+                DispatchQueue.main.async {
+                    imageView.image = image
+                }
+            }
+            task.resume()
+        }
+    }
+
+    func multipleImageViewDidSelect(_ imageView: UIImageView, index: Int) {
+        print("Tapped index: \(index)")
+    }
+}
+
+
